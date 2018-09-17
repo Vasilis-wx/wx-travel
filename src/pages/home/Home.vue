5f3be9a211688000a1bb7295df6dcc31228f934c
wangxin <wangxin@wx.com>
2018-09-17 09:41:24 +0800

    Initial commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="home">Home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+  .home{
+    font-size: 30px;
+  }
+</style>
