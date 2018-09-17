65369ce7d95ce3cd39c2d1e4e76cc7bd306bd5a1
wangxin <wangxin@wx.com>
2018-09-17 19:41:13 +0800

    首页图标组件

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,19 +2,21 @@
   <div>
     <home-header ></home-header>
     <home-swiper ></home-swiper>
-    kajsdhglj
+    <home-icons ></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 
 export default {
   name: 'Home',
   components: {
-    'home-header': HomeHeader,
-    'home-swiper': HomeSwiper
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption" >
+    <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>
-        <img class="swiper-img" src="//ad.qunarzz.com/ad/picture/ba5b0338-ee54-4fa5-a225-3e3c354420b8_%E5%9B%BD%E5%86%85%E8%88%AA%E7%BA%BF.jpg" />
-      </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="//ad.qunarzz.com/ad/picture/a13bf52f-a4d0-403a-9661-2abb173e0259_912-170.jpg" />
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
       </swiper-slide>
       <!-- Optional controls -->
-      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
       <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
       <!--<div class="swiper-button-next" slot="button-next"></div>-->
       <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
@@ -23,6 +20,19 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
+      swiperList: [{
+        id: '0001',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+      }, {
+        'id': '0002',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+      }, {
+        'id': '0003',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+      }, {
+        'id': '0004',
+        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
+      }],
       swiperOption: {
         // 显示指示器
         pagination: '.swiper-pagination',
